954d70c1ab6c4e3427b9006920079ddea6320a6c
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-13 13:20:27 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,7 @@
 class Student < ActiveRecord::Base 
-  def cocatenated 
-    
+  def cocatenated(first_name, last_name)
+    full_name = first_name + last_name
+    full_name.to_s
+    full_name
   end
 end
